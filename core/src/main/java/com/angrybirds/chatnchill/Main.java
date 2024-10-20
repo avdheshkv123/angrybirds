a58ecfee1ce7f39bd2a335e888a64797aaf16dc6
avdheshkv123 <150667845+avdheshkv123@users.noreply.github.com>
2024-10-20 23:57:34 +0530

    Update Main.java

--- core/src/main/java/com/angrybirds/chatnchill/Main.java
+++ core/src/main/java/com/angrybirds/chatnchill/Main.java
@@ -3,6 +3,7 @@ package com.angrybirds.chatnchill;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -13,10 +14,13 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
-
+    Music backgroundmusic;
 
     @Override
     public void create() {
+        backgroundmusic = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        backgroundmusic.setLooping(true);
+        backgroundmusic.play();
         this.setScreen(new Mainscreen(this));
     }
 
