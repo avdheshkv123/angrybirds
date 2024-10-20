1feec42e3705f01f97e1f740ec334eefbad406c0
avdheshkv123 <150667845+avdheshkv123@users.noreply.github.com>
2024-10-20 16:26:54 +0530

    Update Main.java

--- core/src/main/java/com/angrybirds/chatnchill/Main.java
+++ core/src/main/java/com/angrybirds/chatnchill/Main.java
@@ -1,6 +1,7 @@
 package com.angrybirds.chatnchill;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,12 +12,12 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
+public class Main extends Game {
 
 
     @Override
     public void create() {
-
+        this.setScreen(new Mainscreen(this));
     }
 
     @Override
@@ -37,11 +38,11 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void render() {
-
+        super.render();
     }
 
     @Override
     public void dispose() {
-
+        super.dispose();
     }
 }
