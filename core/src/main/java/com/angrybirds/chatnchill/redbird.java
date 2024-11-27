8fcabdc470c66094f1bc609cc40d106924b9dc3c
avdheshkv123 <150667845+avdheshkv123@users.noreply.github.com>
2024-11-27 16:28:00 +0530

    Update redbird.java

--- core/src/main/java/com/angrybirds/chatnchill/redbird.java
+++ core/src/main/java/com/angrybirds/chatnchill/redbird.java
@@ -4,11 +4,29 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class redbird extends bird{
+
     public redbird(float x, float y, float width, float height){
         super(new Texture("redbird.png"),x,y,width,height);
+    }
+
+    public float getX(){
+        return x;
+    }
+
+    public float getY(){
+        return y;
+    }
+
+    public float getWidth(){
+        return width;
+    }
+
+    public float getHeight(){
+        return height;
     }
 
     public void draw(SpriteBatch batch){
         batch.draw(texture,x,y,width,height);
     }
+
 }
