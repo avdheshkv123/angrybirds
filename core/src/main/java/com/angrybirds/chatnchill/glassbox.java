ddefbd5c3b1e382f44f3724c2d13d0f0c8235efd
avdheshkv123 <150667845+avdheshkv123@users.noreply.github.com>
2024-11-27 16:16:19 +0530

    Update glassbox.java

--- core/src/main/java/com/angrybirds/chatnchill/glassbox.java
+++ core/src/main/java/com/angrybirds/chatnchill/glassbox.java
@@ -4,8 +4,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class glassbox extends block{
-    public glassbox(float x, float y, float width, float height){
-        super(new Texture("glassbox.png"),x,y,width,height);
+    public glassbox(float x, float y, float width, float height,int towerid){
+        super(new Texture("glassbox.png"),x,y,width,height,towerid);
     }
 
     public void draw(SpriteBatch batch){
