966a17469de69ad0bbcc06a114595ac72dcce293
avdheshkv123 <150667845+avdheshkv123@users.noreply.github.com>
2024-11-27 16:25:36 +0530

    Update block.java

--- core/src/main/java/com/angrybirds/chatnchill/block.java
+++ core/src/main/java/com/angrybirds/chatnchill/block.java
@@ -6,16 +6,21 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public abstract class block{
     protected Texture texture;
     protected float x,y,width,height;
+    protected int towerID;
+    protected boolean isDestoryed = false;
 
-    public block(Texture texture,float x,float y, float width, float height){
+    public block(Texture texture,float x,float y, float width, float height,int towerID){
         this.texture = texture;
+        this.towerID = towerID;
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
     }
 
-    public abstract void draw(SpriteBatch batch);
+    public void draw(SpriteBatch batch){
+        batch.draw(texture,x,y,width,height);
+    };
 
     public void dispose(){
         texture.dispose();
