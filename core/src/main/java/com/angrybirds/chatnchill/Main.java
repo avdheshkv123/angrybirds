a0f88d764db1f4720cea08ac9b35375f375debac
avdheshkv123 <150667845+avdheshkv123@users.noreply.github.com>
2024-11-29 00:21:59 +0530

    Update Main.java

--- core/src/main/java/com/angrybirds/chatnchill/Main.java
+++ core/src/main/java/com/angrybirds/chatnchill/Main.java
@@ -7,13 +7,19 @@ import com.badlogic.gdx.audio.Music;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     Music backgroundmusic;
+    private boolean isMusicon = true;
+    private float position = 0f;
 
     @Override
     public void create() {
         backgroundmusic = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
         backgroundmusic.setLooping(true);
-        backgroundmusic.play();
-        backgroundmusic.setVolume(0.3f);
+
+        if(isMusicon) {
+            backgroundmusic.play();
+        }
+
+        backgroundmusic.setVolume(0.4f);
         this.setScreen(new frontpage(this));
     }
 
@@ -23,14 +29,50 @@ public class Main extends Game {
 
     }
 
+    public void playMusic(){
+        if(!backgroundmusic.isPlaying()){
+            backgroundmusic.setPosition(position);
+            backgroundmusic.play();
+        }
+    }
+
+    public void stopMusic(){
+        if(backgroundmusic.isPlaying()){
+            position = backgroundmusic.getPosition();
+            backgroundmusic.pause();
+        }
+    }
+
+    public void toggleMusic(){
+        isMusicon = !isMusicon;
+        if(isMusicon){
+            playMusic();
+        }
+        else {
+            stopMusic();
+        }
+    }
+
+    public boolean isMusicon(){
+        return isMusicon;
+    }
+
     @Override
     public void pause() {
         super.pause();
+        if(backgroundmusic.isPlaying()){
+            position = backgroundmusic.getPosition();
+            backgroundmusic.pause();
+        }
     }
 
     @Override
     public void resume() {
         super.resume();
+        if(isMusicon && !backgroundmusic.isPlaying()){
+            backgroundmusic.setPosition(position);
+            backgroundmusic.play();
+        }
     }
 
     @Override
@@ -40,6 +82,7 @@ public class Main extends Game {
 
     @Override
     public void dispose() {
+        backgroundmusic.dispose();
         super.dispose();
     }
 
