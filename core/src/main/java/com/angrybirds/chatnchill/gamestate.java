f02c17348a5020821d8e4945c70fa78403b96ede
avdheshkv123 <150667845+avdheshkv123@users.noreply.github.com>
2024-11-29 23:20:18 +0530

    Update gamestate.java

--- core/src/main/java/com/angrybirds/chatnchill/gamestate.java
+++ core/src/main/java/com/angrybirds/chatnchill/gamestate.java
@@ -12,6 +12,10 @@ public class gamestate{
     public Vector2 birdvelocity;
     public String currentlevel;
 
+    gamestate(){
+
+    }
+
     gamestate(ArrayList<bird> birds, ArrayList<pig> pigs, ArrayList<block> blocks, boolean isbirdreleased,
               Vector2 birdposition, Vector2 birdvelocity, String currentlevel){
         this.birds = birds;
@@ -22,4 +26,12 @@ public class gamestate{
         this.birdvelocity = birdvelocity;
         this.currentlevel = currentlevel;
     }
+
+    public ArrayList<bird> getBirds() { return birds; }
+    public ArrayList<pig> getPigs() { return pigs; }
+    public ArrayList<block> getBlocks() { return blocks; }
+    public boolean isBirdReleased() { return isbirdreleased; }
+    public Vector2 getBirdPosition() { return birdposition; }
+    public Vector2 getBirdVelocity() { return birdvelocity; }
+    public String getLevelName() { return currentlevel; }
 }
